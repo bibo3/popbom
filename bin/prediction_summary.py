d947daa7e967bb60969313bd9300b26fbe444f3d
Matthias <matthias.marquardt@gmx.net>
2021-05-03 21:38:47 +0200

    fix headers in prediction summary

--- bin/prediction_summary.py
+++ bin/prediction_summary.py
@@ -12,8 +12,7 @@ import pandas as pd
 
 def single_df(file):
     with open(file, 'r') as f:
-        table_name = {'mpa': 'metaphlan_table.csv', 'strain': 'metaphlan_marker_table.csv', 'kraken': 'kraken2_table.csv', 'combined': 'strain_species.csv'}
-        phenotype = ['cirrhosis', 'ibd', 'obesity']
+
         meta_name, meta_val = [], []
         metric_name, metric_val = [], []
         prev = 'not empty'
@@ -26,13 +25,7 @@ def single_df(file):
                     if meta[0] == 'Input file':
                         meta_name.append('Experiment')
                         meta_val.append(file.replace('.txt', '').replace('_', ' '))
-                        meta_name.append('dataset')
-                        meta_val.append(''.join([p for p in phenotype if p in meta[1]]))
-                        meta_name.append('taxo profiler')
-                        meta_val.append(''.join([key for key, value in table_name.items() if value in meta[1]]))
-                        
-                        meta_name.append('subdir name')
-                        meta_val.append(meta[1])
+
                     else:
                         meta_name.append(meta[0])
                         meta_val.append(meta[1])
@@ -59,7 +52,7 @@ def single_df(file):
 
 
 def main():
-    path = sys.argv[1]()
+    path = sys.argv[1].split()
     files = [x for x in path if '_roc.txt' not in x]
     files = [x for x in files if '.txt' in x]
     df = pd.concat([single_df(x) for x in files])
